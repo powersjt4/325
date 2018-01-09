06f1e0058007b2b6372dbed52bd0683723ca64bf
powersjt4 <powersj2@oregonstate.edu>
2018-01-08 22:27:12 -0500

    insertsort working mergesort same as insert

--- sort/insertsort.py
+++ sort/insertsort.py
@@ -1,13 +1,27 @@
-filename = "data.txt"
-file = open(filename, "r")
-#for line in file:
-#   print line,
+input = "data.txt"
+output = "insert.out"
+inFile = open(input, "r")
+outFile = open(output, "w")
 
-for line in file:
-	n = 0
-	for n in line.split(): 
-		if n.isdigit():
-			print n
-	print "new line"
+for line in inFile:
+        array = [] 
+        for val in line.split():
+            array.append(int(val))
+	length = int(array[0]) 
+        array.pop(0)
+        print len(array)
 
-file.close()
+        for i in range(1,length): 
+            cur = array[i]
+            prev = i-1 
+            while prev >=0 and cur < array[prev] :
+                array[prev+1] = array[prev]
+                prev -= 1
+            array[prev+1] = cur
+        print(array)
+        for item in array:
+            outFile.write("%d "% item)
+        outFile.write("\n")
+
+inFile.close()
+outFile.close()
--- sort/mergesort.py
+++ sort/mergesort.py
@@ -1,5 +1,27 @@
-filename = "data.txt"
-file = open(filename, "r")
-for line in file:
-   print line,
+input = "data.txt"
+output = "merge.out"
+inFile = open(input, "r")
+outFile = open(output, "w")
 
+for line in inFile:
+        array = [] 
+        for val in line.split():
+            array.append(int(val))
+	length = int(array[0]) 
+        array.pop(0)
+        print len(array)
+
+        for i in range(1,length): 
+            cur = array[i]
+            prev = i-1 
+            while prev >=0 and cur < array[prev] :
+                array[prev+1] = array[prev]
+                prev -= 1
+            array[prev+1] = cur
+        print(array)
+        for item in array:
+            outFile.write("%d "% item)
+        outFile.write("\n")
+
+inFile.close()
+outFile.close()
