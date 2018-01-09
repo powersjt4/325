5ec4e4566edd0ada228b203600a16091694f89b3
powersjt4 <powersjt4@gmail.com>
2018-01-09 11:57:42 -0500

    Merge added to merge sort

--- sort/mergesort.py
+++ sort/mergesort.py
@@ -7,17 +7,12 @@ for line in inFile:
         array = [] 
         for val in line.split():
             array.append(int(val))
-	length = int(array[0]) 
+		length = int(array[0]) 
         array.pop(0)
         print len(array)
-
-        for i in range(1,length): 
-            cur = array[i]
-            prev = i-1 
-            while prev >=0 and cur < array[prev] :
-                array[prev+1] = array[prev]
-                prev -= 1
-            array[prev+1] = cur
+		
+			
+	
         print(array)
         for item in array:
             outFile.write("%d "% item)
@@ -25,3 +20,26 @@ for line in inFile:
 
 inFile.close()
 outFile.close()
+
+#MERGE-SORT A[1 . . n]
+#1.If n = 1, done.
+#2.Recursively sort A[ 1 . . n/2 ] and A[ n/2+1 . . n ] .
+#3.“Merge” the 2 sorted lists.
+
+
+def merge(first, second):
+	mergedList = []
+	while(len(first)>1 and len(second) >1)
+		if(first[0] > second[0])
+			mergedList.append(second[0])
+			del second[0] 
+		else:
+			mergedList.append(first[0])
+			del first[0]
+
+		if len(first) < 1
+			mergedList.append(second[0])
+		else:
+			mergedList.append(first[0])
+	
+	return mergedList
