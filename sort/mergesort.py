3b003cb4d54ee3e3da57f9b76af8327f4c3084e0
powersjt4 <powersjt4@gmail.com>
2018-01-09 12:35:26 -0500

    MergeSort added
    Didn’t work arrays not printing

--- sort/mergesort.py
+++ sort/mergesort.py
@@ -1,3 +1,32 @@
+def merge(first, second):
+	mergedList = []
+	while(len(first)>1 and len(second) >1):
+		if(first[0] > second[0]):
+			mergedList.append(second[0])
+			del second[0] 
+		else:
+			mergedList.append(first[0])
+			del first[0]
+
+		if (len(first) < 1):
+			mergedList.append(second[0])
+		if(len(second) < 1):
+			mergedList.append(second[0])
+	
+	return mergedList
+
+def mergeSort(arr):
+#base case
+	if(len(arr)<2):
+		return arr
+#recursive case
+	mid = len(arr)/2
+	arrT = mergeSort(arr[:mid])
+	arrB = mergeSort(arr[mid:])
+	
+	return merge(arrT, arrB)
+
+#main 
 input = "data.txt"
 output = "merge.out"
 inFile = open(input, "r")
@@ -7,39 +36,13 @@ for line in inFile:
         array = [] 
         for val in line.split():
             array.append(int(val))
-		length = int(array[0]) 
+	#	length = int(array[0]) 
         array.pop(0)
         print len(array)
-		
-			
-	
-        print(array)
+        print(mergeSort([3,4,5,1,2,8,3,7,6]))
         for item in array:
             outFile.write("%d "% item)
         outFile.write("\n")
 
 inFile.close()
 outFile.close()
-
-#MERGE-SORT A[1 . . n]
-#1.If n = 1, done.
-#2.Recursively sort A[ 1 . . n/2 ] and A[ n/2+1 . . n ] .
-#3.“Merge” the 2 sorted lists.
-
-
-def merge(first, second):
-	mergedList = []
-	while(len(first)>1 and len(second) >1)
-		if(first[0] > second[0])
-			mergedList.append(second[0])
-			del second[0] 
-		else:
-			mergedList.append(first[0])
-			del first[0]
-
-		if len(first) < 1
-			mergedList.append(second[0])
-		else:
-			mergedList.append(first[0])
-	
-	return mergedList
