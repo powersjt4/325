7a8aa533ecc045e0214e2df17c2405ccbb7a24a4
powersjt4 <powersj2@oregonstate.edu>
2018-01-09 22:54:13 -0500

    Mergesort works used geeksforgeeks and text book

--- sort/mergesort.py
+++ sort/mergesort.py
@@ -1,51 +1,66 @@
-#MERGEi(A; p; q; r) where A is an array and p, q, and r are indices into the array
-# such that p  q < r.
-def merge(first, second):
-Merge(A,p,q,r)
-	n1 = q-p+1 #find mid point 
-	n2 = r- q
-	L = [] # Left array
-	R = [] # Right array
-	
-	for i = 1 to n1
-		L[i] = A[p+i-1]
+           #p q r
+def merge(A,l,m,r):
+        n1 = m-l+1 
+        n2 = r-m
+        inf = 9999999
+	L = [0] * (n1) # Left array
+	R = [0] * (n2) # Right array
+        #Copy array into L and R	
+        
+        for i in range(0, n1): 
+		L[i] = A[l+i]
 
-	for j = 1 to n2
-		R[j] = A[1+j]
-	L[n1 +1] = inf # sentinal card
-	R[n2 +1] = inf # = infinity
+        for j in range(0, n2): 
+		R[j] = A[m+1+j]
+                
+	L.append(inf) # sentinal card
+	R.append(inf) # = infinity
 
-	i = 1
-	j= 1
-	for k = p to r
-		if(L[i]<= R[j])
-			A[k] = L[i]
-			i = i+1
-		else: 
-			A[k] = R[j]
-			j = j+1
+	i = 0 
+	j = 0 
+        k = l 
+        while i<n1 and j<n2:
+            if L[i] <= R[j]:
+                A[k] = L[i]
+                i+=1
+            else:
+                A[k] = R[j]
+                j +=1
+            k+=1
+    # Copy the remaining elements of L[], if there
+    # are any
+        while i < n1:
+            A[k] = L[i]
+            i += 1
+            k += 1
+     
+    # Copy the remaining elements of R[], if there
+    # are any
+        while j < n2:
+            A[k] = R[j]
+            j += 1
+            k += 1
  
-	return mergedList
+def mergeSort(A,l,r):
+    if l<r:
+	m = (l+(r-1))/2
+	mergeSort(A,l,m)
+	mergeSort(A,m+1,r)
+	merge(A,l,m,r)
 
-Merge-Sort(A,p,r)
-	q = floor(p+r/2)
-	Merege-sort(A,p,q)
-	Merge-sort (A,q+1,r)
-	Merge(A,p,q,r)
+"""
+# Driver code to test above
+arr = [12, 11, 13, 5, 6, 7]
+n = len(arr)
+print ("Given array is")
+for i in range(n):
+    print ("%d" %arr[i]),
  
-
-
-def mergeSort(arr):
-#base case
-	if(len(arr)<2):
-		return arr
-#recursive case
-	mid = len(arr)/2
-	arrT = mergeSort(arr[:mid])
-	arrB = mergeSort(arr[mid:])
-	
-	return merge(arrT, arrB)
-
+mergeSort(arr,0,n-1)
+print ("\n\nSorted array is")
+for i in range(n):
+    print ("%d" %arr[i]),
+"""
 #main 
 input = "data.txt"
 output = "merge.out"
@@ -56,13 +71,12 @@ for line in inFile:
         array = [] 
         for val in line.split():
             array.append(int(val))
-	#	length = int(array[0]) 
-        array.pop(0)
-        print len(array)
-        print(mergeSort([3,4,5,1,2,8,3,7,6]))
+	length = array.pop(0) 
+        print length
+        mergeSort(array,0,length-1)
+        print(array)
         for item in array:
             outFile.write("%d "% item)
         outFile.write("\n")
-
 inFile.close()
 outFile.close()
