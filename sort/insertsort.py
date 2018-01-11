fc08a9a03df3d2ff627d8cef35e7cfe814f45f9b
powersjt4 <powersjt4@gmail.com>
2018-01-11 14:11:59 -0500

    Added input, random array generation and time function

--- sort/insertsort.py
+++ sort/insertsort.py
@@ -1,27 +1,35 @@
-input = "data.txt"
-output = "insert.out"
-inFile = open(input, "r")
-outFile = open(output, "w")
+import time
+import random 
+nums = input("Number of random values ")
+array = []
+for x in range(nums):
+    array.append(random.randint(0,10000))
 
-for line in inFile:
-        array = [] 
-        for val in line.split():
-            array.append(int(val))
-	length = int(array[0]) 
-        array.pop(0)
-        print len(array)
+#input = "data.txt"
+#output = "insert.out"
+#inFile = open(input, "r")
+#outFile = open(output, "w")
+start_time = time.time()
+#for line in inFile:
+#        array = [] 
+#        for val in line.split():
+#            array.append(int(val))
+#	length = int(array[0]) 
+#        array.pop(0)
+#        print len(array)
+length = nums
+for i in range(1,length): 
+    cur = array[i]
+    prev = i-1 
+    while prev >=0 and cur < array[prev] :
+        array[prev+1] = array[prev]
+        prev -= 1
+    array[prev+1] = cur
+print(array)
+#for item in array:
+#    outFile.write("%d "% item)
+#outFile.write("\n")
 
-        for i in range(1,length): 
-            cur = array[i]
-            prev = i-1 
-            while prev >=0 and cur < array[prev] :
-                array[prev+1] = array[prev]
-                prev -= 1
-            array[prev+1] = cur
-        print(array)
-        for item in array:
-            outFile.write("%d "% item)
-        outFile.write("\n")
-
-inFile.close()
-outFile.close()
+print("--- %s seconds ---" % (time.time() - start_time))
+#inFile.close()
+#outFile.close()
