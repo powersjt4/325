defd32c57177e81a76b289ec9e39422d0dd0209a
powersjt4 <powersj2@oregonstate.edu>
2018-01-18 22:53:01 -0500

    added input, random values, and time to stoogesort

--- sort/stoogesort.py
+++ sort/stoogesort.py
@@ -1,20 +1,31 @@
+"""
+This is an implementation of insertion sort that presents the running time
+for average case.
+Other resources: https://rosettacode.org/wiki/Sorting_algorithms/Stooge_sort
+"""
+import time
+import random 
 import math
 def StoogeSort(A,l,h):
-        if l>=h:
-            return
         if (A[l] > A[h]):
             A[l],A[h] = A[h],A[l]
         
-        elif (h-l+1 > 2):
+        if (h-l > 1):
 	    m = int(math.ceil((h-l+1)/3))
 	    StoogeSort(A,l, h-m)
 	    StoogeSort(A,l+m,h)
 	    StoogeSort(A,l, h-m)
 #main
     
-array = [2,3,8,6,1,9]
-
+nums = input("Number of random values: ")
+array = []
+for x in range(nums):
+    array.append(random.randint(0,10000))
+length = nums
+#array = [2,20,22,8,6,1,0,10,9,3]
+start_time = time.time()
 StoogeSort(array,0, len(array)-1)
 for x in array:
     print (x)
+print("--- %s seconds ---" % (time.time() - start_time))
 
