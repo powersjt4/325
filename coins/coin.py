e9a2260b14d377bcc40f9e730c663dc74595550d
powersjt4 <powersj2@oregonstate.edu>
2018-01-25 22:08:53 -0500

    coin outputs to file need to adjust to output coin array

--- coins/coin.py
+++ coins/coin.py
@@ -1,3 +1,10 @@
+"""
+This is an implementation of insertion sort
+from introduction to algorithms, CLRS
+Other resources: https://www.geeksforgeeks.org/insertion-sort/
+"""
+input = "amount.txt"
+output = "change.txt"
 def makeChange(values,chgAmount, count, coinTypes):
 	for i in range (chgAmount+1):
 		coin = 1
@@ -9,7 +16,25 @@ def makeChange(values,chgAmount, count, coinTypes):
 			count[i] = currentCount
 			coinTypes[i] = coin
 	return count[chgAmount]
-
-
+def writeToFile(array, amount, coins):
+    for item in array:
+        outFile.write("%d "% item)
+    outFile.write("\n")
+    outFile.write("%d \n" %amount) 
+        
 #main
-
+inFile = open(input, "r")
+outFile = open(output, "w")
+while True:
+        array = []
+        line = inFile.readline() 
+        if not line: break  # EOF
+        amount = int(inFile.readline())
+        for val in line.split():
+            array.append(int(val))
+        print(amount)
+        print(isinstance(amount, int))
+        print(array)
+        writeToFile(array, amount, array)
+inFile.close()
+outFile.close()
