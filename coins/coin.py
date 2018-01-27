ec5d80b6e50fe2d5d9174f6f52ae11e473501e3d
powersjt4 <powersj2@oregonstate.edu>
2018-01-27 17:05:16 -0500

    Working need to investigate makeChange function

--- coins/coin.py
+++ coins/coin.py
@@ -1,14 +1,14 @@
 """
 This is an implementation of insertion sort
 from introduction to algorithms, CLRS
-Other resources: https://www.geeksforgeeks.org/insertion-sort/
+Other resources: http://interactivepython.org/runestone/static/pythonds/Recursion/DynamicProgramming.html
 """
 input = "amount.txt"
 output = "change.txt"
 
 def makeChange(V,A,T,C):
-	for i in range (A+1):
-		coin = 1
+	for i in range (A+1):#loop through amount
+		coin = 1 
 		currentCount = i
 		for j in [c for c in V if c <= i]:
 	 		if (T[i-j] + 1) < currentCount:
@@ -17,15 +17,27 @@ def makeChange(V,A,T,C):
 			T[i] = currentCount
 			C[i] = coin
 	return T[A]
-
-
+"""
+Writes all the information to the output file 
+calculates the denomenations used and creates a new array
+for track coin use.
+"""
 def writeToFile(array, amount, coins):
     for item in array:
         outFile.write("%d "% item)
     outFile.write("\n")
     outFile.write("%d \n" %amount) 
-    for coin in coins:
-        outFile.write("%d "% coin)
+    countCoins(array,amount, coins)   
+    coin = amount 
+    coinCnt = [0]*len(array)#Array same size as original denomenations
+    while coin > 0:
+        current = coins[coin]
+        for i in range(len(array)):
+            if(array[i] == current): #Searches array to find instance of current
+                coinCnt[i]+=1 
+        coin = coin - current#subtracts current value to get next value
+    for item in coinCnt:
+        outFile.write("%d "% item)
     outFile.write("\n\n")
 
 #main
