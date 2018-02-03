84c0f0caa0eeda34d4cfc9695ec5aa5048520d3c
powersjt4 <powersj2@oregonstate.edu>
2018-02-02 21:54:37 -0500

    kind of works not getting correct values for first set and they are print in reverse order.

--- sched/sched.py
+++ sched/sched.py
@@ -5,18 +5,35 @@ Other resources: https://www.geeksforgeeks.org/insertion-sort/
 """
 input = "act.txt"
 inFile = open(input, "r")
-
+"""
+#Greedy choice is to select the last to start
 #A[n][0] = act. # a[n][1] = Start a[n][2] = Finish
 def activitySchedule(A,length):
-	for i in range(1,int(length)): 
-		prev = A[i-1]
-#		print(A[i][1])
-#		print( A[i-1][2])
-	
-		if(A[i][1] <= A[i-2][2] ):#S of next >= f of prev
-			print(A[i])
-	
+    for i in range(0,int(length)-1): 
+        next = A[i+1]
+        cur = A[i]
+        if(next[1] > cur[1] and next[2] >= cur[1]):
+            if(next[2]>=cur[1]):
+                print(next)
+        elif(cur[1] > prev[1] and cur[2]>=next[2]):
+            print(cur)
 
+            #print("Start", A[i][1]," <= start 2",prev[1])
+		#f(A[i][1] >= prev[1] and A[i][1]>=prev[2]):#S of next >= f of prev
+		#    print(A[i])
+                #elif(prev[1] >= A[i][1] and prev[2]>= A[i][1]):
+                #    print(prev)    
+"""                
+
+ 
+def activitySchedule(A,length):	
+    print(A[0])
+    k = 1
+    for i in range(1, int(length)):
+        if(A[i][2] <= A[k][1]):  
+            print(A[i])
+            k = i
+        
 while True:
     A = []
     arrLen = inFile.readline()#Read in length, first line
@@ -29,19 +46,19 @@ while True:
 
     print(A)
 #Insertion sort sort whole row by the last value the finish time.
+# A[i][1] = sort on start time, A[i][2] = sort on finish time    
     for i in range(1, int(arrLen)):
-        key = A[i][2]
-        prev = A[i] 
-        j = i - 1
-        while (j >= 0 and key < A[j][1]):
-            A[j+1] = A[j]
-            j-= 1
-        A[j+1] = prev 
-	activitySchedule(A,arrLen)		
+         key = A[i][1]
+         prev = A[i] 
+         j = i - 1
+         while (j >= 0 and key > A[j][1]):# > Desc. < ascend
+             A[j+1] = A[j]
+             j-= 1
+         A[j+1] = prev   
+    activitySchedule(A,arrLen)		
 
 
     print(A)
-
     print("\n\n")
 inFile.close()
 
