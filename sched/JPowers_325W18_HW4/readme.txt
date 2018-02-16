33a4d0bae1084255f1b3e86a456bea77174b72ce
powersjt4 <powersjt4@gmail.com>
2018-02-16 13:57:47 -0500

    Small changes

---
+++ sched/JPowers_325W18_HW4/readme.txt
@@ -0,0 +1,6 @@
+sched.py reads in the file act.txt and prints ro terminal
+Need to type python “filename.py”
+
+
+Thanks.
+
--- wrestle/wrestle.py
+++ wrestle/wrestle.py
@@ -5,7 +5,7 @@ import itertools
 from sys import argv
 script, inFilename = argv
 #inFile = open(inFilename, "r")
-wDict = {}
+G = {}
 wArr = []
 eArr = []
 
@@ -14,35 +14,18 @@ with open(inFilename, "r") as inFile:
 	for i in range(0,numW):
 		wArr.append(inFile.readline())
 	wArr = [x.rstrip() for x in wArr]
-	wDict = dict((x,[]) for x in wArr)
+	G = dict((x,[]) for x in wArr)
 	numE = int(inFile.readline())
 	for line in range(0, numE):
 		edge = inFile.readline().split()
 		print edge[0]
-		wDict[edge[0]].append(edge[1])
-		wDict[edge[1]].append(edge[0])
+		G[edge[0]].append(edge[1])
+		G[edge[1]].append(edge[0])
 #print wArr
-#print wDict 
-
-# visits all the nodes of a graph (connected component) using BFS
-def bfs_connected_component(graph, start):
-    # keep track of all visited nodes
-    explored = []
-    # keep track of nodes to be checked
-    queue = [start]
- 
-    # keep looping until there are nodes still to be checked
-    while queue:
-        # pop shallowest node (first node) from queue
-        node = queue.pop(0)
-        if node not in explored:
-            # add node to list of checked nodes
-            explored.append(node)
-            neighbours = graph[node]
- 
-            # add neighbours of node to queue
-            for neighbour in neighbours:
-                queue.append(neighbour)
-    return explored
- 
-bfs_connected_component(wDict,'Bear') # returns ['A', 'B', 'C', 'E', 'D', 'F', 'G']
+#for key in G:
+#    G[key].append([0,2])
+print G['Duke'] 
+"""
+New plan create the BFS but add the key of the baby or heel to
+a seperate list.
+"""
